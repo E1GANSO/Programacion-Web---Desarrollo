6e6f5c88524212c5bb340be2901f95a17ce8da81
E1GANSO <alvarojavierpedrozopena@gmail.com>
2021-10-09 09:02:15 -0500

    Actualizacion de Cursos_2

---
+++ Pildoras_Informaticas/JavaScript/Inicio/Funcion_Inicial.js
@@ -0,0 +1,74 @@
+
+/*
+     ----- EJERCICIO 1 ----- JAVA SCRIPT -----
+
+
+
+    A) 3 chicos de 23 años perfectamente normales entran a una heladeria a comprar un helado
+    pero hay un problema: Los precios no estan al lado de cada estante con su respectivo helado.
+    Ellos quieren comprar el helado mas caro que puedan con el dinero que tienen, asi que... Veamos como ayudarlo.
+
+    ROBERTO tiene $1.5 USD
+    PEDRO tiene $1.7 USD
+    Cofla tiene $3 USD
+
+    Los precios de helados son los siguientes:
+    Palito de helado de agua: $0.6 USD
+    Palito de helado de crema: $1 USD
+    Bombón de helado heladix: $1.6 USD
+    Bombón de helado heladovich: $1.7 USD
+    Bombón de helado Helardo: $1.8 USD
+    Potecito de helado con confites: $2.9 USD
+    Pote de 1/4 Kgr -> $2.9 USD
+
+
+
+		CREAR SOLUCIONES:
+			- Pedirles que ingresen el monto que tienen y mostrarles el helado mas caro que puedan comprar
+			- Si hay dos o mascon el mismo precio, mostrar ambos.
+			- Cofla quiere saber cuanto es el vuelto.
+
+*/
+
+/*
+let monto = prompt("Ingrese su monto");
+if(monto >0 && monto <= 1.1){
+    alert("Helados Disponobles:\nPalito de helado de agua: $0.6 USD\nPalito de helado de crema: $1 USD");
+}
+else if(monto > 1.1 && monto <= 1.7){
+    alert("Helados Disponobles\nBombón de helado heladovich: $1.7 USD\nBombón de helado heladix: $1.6 USD\nPalito de helado de crema: $1 USD\nPalito de helado de agua: $0.6 USD");
+}
+else if(monto > 1.7){
+    alert("Helados Disponibles:\nPalito de helado de agua: $0.6 USD\nPalito de helado de crema: $1 USD\nBombón de helado heladix: $1.6 USD\nBombón de helado heladovich: $1.7 USD\nBombón de helado Helardo: $1.8 USD\nPotecito de helado con confites: $2.9 USD\nPote de 1/4 Kgr -> $2.9 USD");
+}
+else{
+    alert("No hay dinero suficiente");
+}
+*/
+
+/*
+//            ------- USO DE ARRAY -------
+
+Array = [1,5,6,4,8];
+alert(Array);
+
+//            ------- Array de Objetos -------
+
+let PC = {
+    nombre: "PC MASTER RACE",
+    RAM: "16GB",
+    Procesador: "Icore i5 2400",
+    Disco_Duro: "500GB HDD",
+    Tarjeta_De_Video: "Nvidia GT 710"
+}
+*/
+
+
+//      -------- Ciclo (For IN) Nos devuelve la posicion --------
+/*
+let cocina = ["plato", "Estufa", "Nevera", "Cucharas"];
+
+for (Recor_Cocina of cocina) {
+    document.write(Recor_Cocina + "<br>");
+}
+*/
